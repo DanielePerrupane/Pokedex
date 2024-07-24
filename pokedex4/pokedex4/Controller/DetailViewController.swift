9243b9129400e1fb15d6252e16612efb19573835
DanielePerrupane <148336607+DanielePerrupane@users.noreply.github.com>
2024-07-24 14:55:28 +0200

    risolto l'erro con i pokemon con 1 solo tipo aggiungendo un semplice controllo sulla count dell'arrai di types, impostato un layout base per la visualizzazione delle due type label ma ancora non è risolto il padding e la visualizzazione corretta delle typelabel

--- pokedex4/pokedex4/Controller/DetailViewController.swift
+++ pokedex4/pokedex4/Controller/DetailViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 class DetailViewController: UIViewController {
     
     //Contenitore per i dati dell'elemento da visualizzare
-    var item: (image: UIImage, nameLabel: String, typeLabel: String, flavorTextLabel: String)?
+    var item: (image: UIImage, nameLabel: String, typeLabel1: String, typeLabel2: String, flavorTextLabel: String)?
     
-    //Elementi UI
-    private var imageView: UIImageView = {
-            let imageView = UIImageView()
-            imageView.translatesAutoresizingMaskIntoConstraints = false
-            imageView.contentMode = .scaleAspectFit
-            return imageView
-        }()
+        //Elementi UI
+        private var imageView: UIImageView = {
+                let imageView = UIImageView()
+                imageView.translatesAutoresizingMaskIntoConstraints = false
+                imageView.contentMode = .scaleAspectFit
+                return imageView
+            }()
         
         private var nameLabel: UILabel = {
             let nameLabel = UILabel()
@@ -28,11 +28,24 @@ class DetailViewController: UIViewController {
             return nameLabel
         }()
         
-        private var typeLabel: UILabel = {
-            let typeLabel = UILabel()
-            typeLabel.translatesAutoresizingMaskIntoConstraints = false
-            typeLabel.textAlignment = .center
-            return typeLabel
+        private var typeLabel1: UILabel = {
+            let typeLabel1 = UILabel()
+            typeLabel1.translatesAutoresizingMaskIntoConstraints = false
+            typeLabel1.textAlignment = .center
+            typeLabel1.layer.borderColor = UIColor.white.cgColor
+            typeLabel1.layer.borderWidth = 1.0
+            typeLabel1.layer.cornerRadius = 8
+            return typeLabel1
+        }()
+    
+        private var typeLabel2: UILabel = {
+            let typeLabel2 = UILabel()
+            typeLabel2.translatesAutoresizingMaskIntoConstraints = false
+            typeLabel2.textAlignment = .center
+            typeLabel2.layer.borderColor = UIColor.white.cgColor
+            typeLabel2.layer.borderWidth = 1.0
+            typeLabel2.layer.cornerRadius = 8
+            return typeLabel2
         }()
         
         private var flavorTextLabel: UILabel = {
@@ -51,33 +64,39 @@ class DetailViewController: UIViewController {
         //Aggiungiamo gli elementi alla UI
         view.addSubview(imageView)
         view.addSubview(nameLabel)
-        view.addSubview(typeLabel)
+        view.addSubview(typeLabel1)
+        view.addSubview(typeLabel2)
         view.addSubview(flavorTextLabel)
         
         NSLayoutConstraint.activate([
-                 imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-                 imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                 imageView.widthAnchor.constraint(equalToConstant: 200),
-                 imageView.heightAnchor.constraint(equalToConstant: 200),
-                 
-                 nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
-                 nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-                 nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-                 
-                 typeLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 20),
-                 typeLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-                 typeLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-                 
-                 flavorTextLabel.topAnchor.constraint(equalTo: typeLabel.bottomAnchor, constant: 20),
-                 flavorTextLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-                 flavorTextLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-             ])
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 200),
+            
+            nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            typeLabel1.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 20),
+            typeLabel1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 145),
+            typeLabel1.trailingAnchor.constraint(equalTo: typeLabel2.leadingAnchor, constant: -10),
+            
+            typeLabel2.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 20),
+            typeLabel2.leadingAnchor.constraint(equalTo: typeLabel1.trailingAnchor, constant: 10),
+            typeLabel2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -145),
+            
+            flavorTextLabel.topAnchor.constraint(equalTo: typeLabel1.bottomAnchor, constant: 20),
+            flavorTextLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            flavorTextLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        ])
         
         // Imposta i dati dell'elemento
         if let item = item {
             imageView.image = item.image
             nameLabel.text = item.nameLabel
-            typeLabel.text = item.typeLabel
+            typeLabel1.text = item.typeLabel1
+            typeLabel2.text = item.typeLabel2
             flavorTextLabel.text = item.flavorTextLabel
             
         }
@@ -88,10 +107,18 @@ class DetailViewController: UIViewController {
     func configure(with pokemonDetail: Pokemon) {
         nameLabel.text = pokemonDetail.name.capitalized
         nameLabel.font = UIFont.boldSystemFont(ofSize: 30)
-//        typeLabel.text = pokemonDetail.types.map { $0.type.name }.joined(separator: ", ").capitalized
-        flavorTextLabel.text = pokemonDetail.flavorText
-          
+        
+        if (pokemonDetail.types.count == 2) {
+            typeLabel1.text = pokemonDetail.types[0].type?.name
+            typeLabel2.text = pokemonDetail.types[1].type?.name
+        } else {
+            typeLabel1.text = pokemonDetail.types[0].type?.name
         }
+        
+        flavorTextLabel.text = pokemonDetail.flavorText
+        
+        
+    }
     
     //Metodo per configurare l'immagine della cella di dettaglio
     func configureImage(with imageUrlString: String) {
